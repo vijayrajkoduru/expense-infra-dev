1009f5736d72a1fe006c1e934994851d5a9696d9
Vijaykumar-Koduru <koduru.vijaykumar.7@gmail.com>
2025-06-25 03:01:28 +0530

    new folder created

---
+++ 10-security-group/sg-test.tf
@@ -0,0 +1,70 @@
+module "mysql_sg" {
+    source = "git::https://github.com/vijayrajkoduru/terraform-aws-SG.git?ref=main"
+    project_name = var.project_name
+    environment = var.environment
+    sg_name = "mysql"
+    sg_description = "Created for MySQL instances in expense dev"
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    common_tags = var.common_tags
+}
+
+module "backend_sg" {
+    source ="git::https://github.com/vijayrajkoduru/terraform-aws-SG.git?ref=main"
+    project_name = var.project_name
+    environment = var.environment
+    sg_name = "backend"
+    sg_description = "Created for backend instances in expense dev"
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    common_tags = var.common_tags
+}
+
+module "frontend_sg" {
+    source = "git::https://github.com/vijayrajkoduru/terraform-aws-SG.git?ref=main"
+    project_name = var.project_name
+    environment = var.environment
+    sg_name = "frontend"
+    sg_description = "Created for frontend instances in expense dev"
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    common_tags = var.common_tags
+}
+
+module "bastion_sg" {
+    source = "git::https://github.com/vijayrajkoduru/terraform-aws-SG.git?ref=main"
+    project_name = var.project_name
+    environment = var.environment
+    sg_name = "bastion"
+    sg_description = "Created for bastion instances in expense dev"
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    common_tags = var.common_tags
+}
+
+module "app_alb_sg" {
+    source = "git::https://github.com/vijayrajkoduru/terraform-aws-SG.git?ref=main"
+    project_name = var.project_name
+    environment = var.environment
+    sg_name = "app_alb"
+    sg_description = "Created for backend ALB in expense dev"
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    common_tags = var.common_tags
+}
+
+resource "aws_security_group_rule" "app_alb_bastion" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol = "tcp"
+  source_security_group_id   = module.bastion_sg.sg_id
+  security_group_id = module.app_alb_sg.sg_id
+}
+
+resource "aws_security_group_rule" "bastion_public" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol = "tcp"
+  cidr_blocks       = ["139.5.249.162/32"]
+  security_group_id = module.bastion_sg.sg_id
+
+
+}
+
