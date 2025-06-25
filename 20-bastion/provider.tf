80730aebd23a9e5db8463352372ce83a473f44c4
Vijaykumar-Koduru <koduru.vijaykumar.7@gmail.com>
2025-06-25 15:58:09 +0530

    created

---
+++ 20-bastion/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.84.0"
+    }
+  }
+
+  
+  backend "s3" {
+    bucket         = "expense-project-1"
+    key            = "expense-VPN"
+    region         = "us-east-1"
+    dynamodb_table = "expense-project-dynamo"
+  }
+}
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
